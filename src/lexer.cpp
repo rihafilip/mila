108ce4742a5cfc012e16e2ff4739cd8a78490381
Filip Riha <riha.fina@gmail.com>
2022-04-25 13:24:38 +0200

    Refactor lexer

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -3,124 +3,116 @@
 #include "tokens.hpp"
 #include "variant_helpers.hpp"
 #include <cctype>
-#include <functional>
-#include <iostream>
 #include <optional>
 #include <stdexcept>
-#include <string>
 
 using namespace token;
 
 
 namespace lexer
 {
+    /// Bool wrapper struct that signifies if this is a start state
+    template <typename St>
+    struct is_start_state {
+        constexpr static bool value = false;
+    };
+
+    template <>
+    struct is_start_state<S> {
+        constexpr static bool value = true;
+    };
+
     /***********************************************/
 
-    void Lexer::skip_ws()
+    /// Get a state, its transition table and a character and make a transition
+    template <typename St>
+    std::optional<token::Token> transition ( const Table<St>& table, std::istream& stream, const St& state )
     {
-        while ( m_Data.good() && std::isspace( m_Data.peek() ) ){
-            m_Data.get();
+        // Bad stream
+        if ( stream.fail() ){
+            throw std::runtime_error( "Error while reading input." );
+        }
+
+        // Handle EOF
+        if ( stream.eof() || std::isspace( stream.peek() ) ){
+            // On start state just return nothing
+            if ( is_start_state<St>::value ){
+                return std::nullopt;
+            }
+
+            // Try to extract a token from state, fail if there is none
+            auto tk = table.m_ExtractToken( state );
+            if ( tk.has_value() ){
+                return tk.value();
+            }
+            else {
+                throw std::runtime_error( "Unexpected end of input." );
+            }
+        }
+
+        // Get next char
+        char ch = stream.peek();
+
+        // Try to find in map
+        if ( auto i = table.m_Map.find( ch ); i != table.m_Map.end() ){
+            stream.get();
+            auto token_or_state = i->second( state, ch );
+
+            // Transition more on state, return token on token
+            return wrap(token_or_state).visit(
+                []( const Token& tk ) -> std::optional<token::Token>
+                {
+                    return tk;
+                },
+                [&stream] ( const State& new_state )
+                {
+                    return transition_all( new_state, stream );
+                }
+            );
+        }
+
+        // Try to extract token on character that's not in table
+        else if ( auto tok = table.m_ExtractToken( state ); tok.has_value() ) {
+            return tok.value();
+        }
+
+        // Error
+        else {
+            throw std::runtime_error( std::string {"Unexpected character "} + ch );
         }
     }
 
-    /***********************************************/
-
-    /**
-     * @brief Visit all states with table and function
-     *
-     * @param fun Function returning visiting function on passing table
-     * @param st State to visit
-     * @return auto Output of fun on visiting
-     */
-    auto visit_with_tables ( const auto& fun, const State& st ){
-        return wrap( st ).visit(
-            fun(S_TABLE),
-            fun(DECIMAL_TABLE),
-            fun(OCTAL_START_TABLE),
-            fun(OCTAL_TABLE),
-            fun(HEX_START_TABLE),
-            fun(HEX_TABLE),
-            fun(WORD_TABLE),
-            fun(GREATER_THAN_TABLE),
-            fun(LOWER_THAN_TABLE),
-            fun(COLON_TABLE)
-        );
-    }
-
-    std::optional<token::Token> Lexer::handleEof()
+    std::optional<token::Token> transition_all ( const State& state, std::istream& stream )
     {
-        // On start state, don't try to extract anything
-        if ( wrap(m_State).get<S>().has_value() ){
-            return std::nullopt;
-        }
-
-        // Extracting function
-        auto lam = []<typename St>( const Table<St> table ){
-            return [table]( const St& state ){
-                    return table.m_ExtractToken( state );
+        auto lam = [&stream] <typename St> ( const Table<St>& table ){
+            return [&stream, &table] ( const St& state ){
+                return transition( table, stream, state );
             };
         };
 
-        std::optional<token::Token> tk = visit_with_tables(lam, m_State);
-        if ( tk.has_value() ) {
-            return tk.value();
-        }
-        else {
-            throw std::runtime_error( "Unexpected end of input." );
-        }
+        return wrap( state ).visit(
+            lam(S_TABLE),
+            lam(DECIMAL_TABLE),
+            lam(OCTAL_START_TABLE),
+            lam(OCTAL_TABLE),
+            lam(HEX_START_TABLE),
+            lam(HEX_TABLE),
+            lam(WORD_TABLE),
+            lam(GREATER_THAN_TABLE),
+            lam(LOWER_THAN_TABLE),
+            lam(COLON_TABLE)
+        );
     }
 
     /***********************************************/
 
     std::optional<token::Token> Lexer::next()
     {
-        skip_ws();
-
-        if ( m_Data.fail() ) {
-            throw std::runtime_error( "Error while reading input." );
+        // Skip whitespaces
+        while ( m_Data.good() && std::isspace( m_Data.peek() ) ){
+            m_Data.get();
         }
 
-        if ( m_Data.eof() ) {
-            return handleEof();
-        }
-
-        char ch = m_Data.peek();
-
-        // visiting function
-        const auto lam = [ch, this] <typename St> ( const Table<St>& table )
-        {
-            return [ch, this, table] ( const St& state ) -> TransitionReturn
-            {
-                auto i = table.m_Map.find(ch);
-                // found in map
-                if ( i != table.m_Map.end() ){
-                    this -> m_Data.get();
-                    return i->second(state, ch);
-                }
-                // move to otherwise function, token extracted
-                else if ( auto tok = table.m_ExtractToken( state ); tok.has_value() ) {
-                    return tok.value();
-                }
-                // error
-                else{
-                    throw std::runtime_error( std::string {"Unexpected character "} + ch );
-                }
-            };
-        };
-
-        TransitionReturn ret = visit_with_tables(lam, m_State);
-
-        // either new state or token and reset to state to S
-        return wrap(ret).visit(
-            [this]( const Token& tk) -> std::optional<token::Token>
-            {
-                this->m_State = start_state();
-                return tk;
-            },
-            [this] ( const State& st ){
-                this->m_State = st;
-                return this->next();
-            }
-        );
+        return transition_all(start_state(), m_Data);
     }
 }
--- src/lexer.hpp
+++ src/lexer.hpp
@@ -8,14 +8,19 @@
 
 namespace lexer
 {
+    /**
+     * @brief Get a character from stream and transition with it on the given state
+     *
+     * @param state State to transition from
+     * @param stream Stream to get characters from
+     * @return std::optional<token::Token> Token or nothing on EOF
+     */
+    std::optional<token::Token> transition_all ( const State& state, std::istream& stream );
+
     class Lexer
     {
     private:
         std::istream& m_Data;
-        State m_State = start_state(); // start symbol
-
-        void skip_ws();
-        std::optional<token::Token> handleEof();
 
     public:
         Lexer( std::istream& str )
@@ -29,6 +34,5 @@ namespace lexer
     };
 
 }
-
 
 #endif // LEXER_HPP
